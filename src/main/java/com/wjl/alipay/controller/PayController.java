c45a5c73aadce5752ad813872b0dff82f2653065
fsadjkjl <904822026@qq.com>
2017-11-16 15:40:33 +0800

    .0.0.

--- src/main/java/com/wjl/alipay/controller/PayController.java
+++ src/main/java/com/wjl/alipay/controller/PayController.java
@@ -1,6 +1,5 @@
 package com.wjl.alipay.controller;
 
-import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 
@@ -11,7 +10,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.alipay.api.AlipayApiException;
